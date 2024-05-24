b1df4e5eea7c069a8ae43f6bf4a5a31f89ff2eb6
reeny404 <happy2hyeon@kakao.com>
2024-05-24 20:45:14 +0900

    폴더명 변경

--- assignment/03-2-redux/src/main.jsx
+++ assignment/03-2-redux/src/main.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom/client.js";
+import { Provider } from "react-redux";
 import App from "./App.jsx";
 import "./index.css";
-import { Provider } from "react-redux";
-import store from "./redux/store";
+import store from "./redux/store.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
