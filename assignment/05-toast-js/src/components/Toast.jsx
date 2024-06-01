b186dac1c6d2a453585a63f6ef666f347e1daad7
reeny404 <happy2hyeon@kakao.com>
2024-06-01 19:06:08 +0900

    useEffect 상단으로 올리기

--- assignment/05-toast-js/src/components/Toast.jsx
+++ assignment/05-toast-js/src/components/Toast.jsx
@@ -4,17 +4,17 @@ import { useEffect, useState } from "react";
 function Toast({ toast }) {
   const [isShow, setIsShow] = useState(false);
 
-  const className = clsx(
-    "w-64 rounded shadow-lg py-5 px-4 text-left border-gray-300 transition duration-500",
-    isShow && !toast.isDone ? "-translate-x-full" : "translate-x-4"
-  );
-
   useEffect(() => {
     setIsShow(true);
   }, []);
 
   return (
-    <div className={className}>
+    <div
+      className={clsx(
+        "w-64 rounded shadow-lg py-5 px-4 text-left border-gray-300 transition duration-500",
+        isShow && !toast.isDone ? "-translate-x-full" : "translate-x-4"
+      )}
+    >
       <h3 className="text-sm font-bold">{toast.title}</h3>
       <p className="text-xs">{toast.desc}</p>
     </div>
